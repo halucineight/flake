f0ae3d3a1f5fad93209dd9c64e662c8491ce40b6
halucineight <halucineight@users.noreply.github.com>
2026-04-02 15:38:41 +0000

    Add bootloader settings

---
+++ modules/bootloader/default.nix
@@ -0,0 +1,13 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.systemd-boot.configurationLimit = 3;
+
+}
