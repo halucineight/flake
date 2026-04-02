843d95e92aee9a3580f4fa681b4c705428357dcc
halucineight <halucineight@users.noreply.github.com>
2026-04-02 20:40:12 +0000

    Add nvtop for desktop and a comment

--- machines/desktop/default.nix
+++ machines/desktop/default.nix
@@ -1,10 +1,9 @@
 # Desktop machine-specific configuration
-{
-  config,
-  pkgs,
-  lib,
-  ags,
-  ...
+{ config
+, pkgs
+, lib
+, ags
+, ...
 }:
 
 {
@@ -17,14 +16,20 @@
     ../../modules/home-manager
   ];
 
-  # DeepCool hardware control (optional)
-  services.hardware.deepcool-digital-linux =  {
+  #Packages and services specific to this computer will go here:
+  #If the list gets too long I'll create a new file
+  environment.systemPackages = with pkgs; [
+    nvtopPackages.nvidia
+  ];
+
+  services.hardware.deepcool-digital-linux = {
     enable = true;
     extraArgs = [
       "--mode"
       "auto"
     ];
   };
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
 }
