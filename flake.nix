24a019f4d5930b60fa0005cc140e69254bafbf5f
halucineight <halucineight@users.noreply.github.com>
2026-04-03 17:19:23 +0530

    Install quickshell

--- flake.nix
+++ flake.nix
@@ -9,6 +9,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    quickshell = {
+      # add ?ref=<tag> to track a tag
+      url = "github:quickshell-mirror/quickshell";
+
+      # THIS IS IMPORTANT
+      # Mismatched system dependencies will lead to crashes and other issues.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     astal = {
       url = "github:aylur/astal";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -36,6 +45,7 @@
     , nixpkgs
     , home-manager
     , ags
+    , quickshell
     , dotfiles
     , ags-shell
     , ...
@@ -44,7 +54,14 @@
       nixosConfigurations = {
         desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit ags dotfiles ags-shell; };
+          specialArgs = {
+            inherit
+              ags
+              dotfiles
+              ags-shell
+              ;
+            quickshellFlake = quickshell;
+          };
           modules = [
             ./configuration.nix
             ./machines/desktop
