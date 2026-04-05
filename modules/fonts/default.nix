134afa172b93ab006029c3e58a3f811583e17d48
halucineight <halucineight@users.noreply.github.com>
2026-04-05 18:23:54 +0530

    Add fonts

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -4,6 +4,9 @@
   fonts.packages = with pkgs; [
     nerd-fonts.meslo-lg
     meslo-lgs-nf
+    nerd-fonts._3270
+    nerd-fonts.hack
+    nerd-fonts._0xproto
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
