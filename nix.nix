f409308aa391560f9fde51671b76a63feeb3b314
halucineight <halucineight@users.noreply.github.com>
2026-05-04 20:13:33 +0530

    Add CUDA cache

--- nix.nix
+++ nix.nix
@@ -1,15 +1,25 @@
-{ config
-, lib
-, pkgs
-, self
-, ...
+{
+  config,
+  lib,
+  self,
+  ...
 }:
 
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://cache.nixos-cuda.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+    ];
+  };
 
   nix.registry.myflake.flake = self;
 
@@ -18,6 +28,6 @@
     dates = "weekly"; # systemd.time(7) format
     options = "--delete-older-than 3d";
   };
-  
+
   nixpkgs.config.allowUnfree = true;
 }
