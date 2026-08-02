5bf4139e4393e00fd28dd8236c6fbc99979b1744
halucineight <halucineight@users.noreply.github.com>
2026-08-02 08:09:41 +0000

    Use latest kernel for ph315

--- machines/ph315/default.nix
+++ machines/ph315/default.nix
@@ -1,5 +1,11 @@
 # Laptop machine-specific configuration
-{ config, pkgs, lib, ags, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ags,
+  ...
+}:
 
 {
   networking.hostName = "ph315";
@@ -11,6 +17,11 @@
     ../../modules/home-manager
   ];
   # Use lts kernel.
-  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  programs.nh = {
+    enable = true;
+    flake = "path:/home/${config.users.primaryUser}/flake";
+  };
 
 }
