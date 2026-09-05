9a2536be430595fed339d5feba2bafb00d405695
halucineight <halucineight@users.noreply.github.com>
2026-09-05 19:24:35 +0000

    Disable wifi powersave

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -1,12 +1,13 @@
-{ config
-, lib
-, pkgs
-, ...
+{
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 
 {
   networking.hostName = lib.mkDefault "nixos";
   networking.networkmanager.enable = true;
-
+  networking.networkmanager.wifi.powersave = false;
   networking.firewall.enable = true;
 }
